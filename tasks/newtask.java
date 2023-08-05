725a6e55cc31774f3b97e26b39b540d7dbd6e1b0
Velimira <revekka-best@mail.ru>
2023-08-05 11:07:09 +0300

    HWL

---
+++ tasks/newtask.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class newtask {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите данные (Фамилия Имя Отчество дата рождения номер телефона пол): ");
+        String input = scanner.nextLine();
+
+        String[] data = input.split(" ");
+
+        if (data.length != 6) {
+            System.out.println("Ошибка! Введено неверное количество данных.");
+            return;
+        }
+
+        try {
+            String surname = data[0];
+            String firstName = data[1];
+            String middleName = data[2];
+            String birthDate = data[3];
+            String phoneNumber = data[4];
+            String gender = data[5];
+
+            String output = surname + firstName + middleName + birthDate + " " + phoneNumber + gender;
+
+            FileWriter fileWriter = new FileWriter(surname + ".txt");
+            fileWriter.write(output);
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Ошибка! Не удалось создать файл.");
+            return;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка! Введены неполные данные.");
+            return;
+        }
+
+        System.out.println("Данные успешно сохранены в файле " + data[0] + ".txt");
+    }
+}
